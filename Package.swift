c13f3d9c5ff7c5e8ee2f5ca91b3a5c07f77a9818
Dmitrii Kuznetsov <dsk1306@icloud.com>
2023-06-18 13:11:09 +0200

    Updated CI Actions (#4)

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 5.7
 import PackageDescription
 
 let package = Package(
     name: "SwiftUIExtensions",
     platforms: [
         .iOS(.v15),
-        .macOS(.v12),
+        .macOS(.v13),
         .macCatalyst(.v15),
         .tvOS(.v15),
         .watchOS(.v8)
